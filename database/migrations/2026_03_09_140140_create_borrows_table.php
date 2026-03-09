c30e1b85063d5b522303fab1dc1e79fd95f7307e
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-09 14:35:00 +0000

    feat : add borrow migration

---
+++ database/migrations/2026_03_09_140140_create_borrows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('copy_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('borrow_date');
+            $table->dateTime('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrows');
+    }
+};
