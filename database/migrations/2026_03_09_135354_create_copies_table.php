3df25d6bc3feff94c1dcf79debe9a6be101cf740
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-09 14:34:44 +0000

    feat : add copies migration

---
+++ database/migrations/2026_03_09_135354_create_copies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('copies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cookbook_id')->constrained()->cascadeOnDelete();
+            $table->enum('statut', ['available', 'borrowed', 'lost']);
+            $table->enum('condition', ['good', 'damaged', 'missing_pages']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('copies');
+    }
+};
