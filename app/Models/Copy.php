3f0cfa434cbc5bead719c85b8ad0c835fc5f5453
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-09 14:33:32 +0000

    feat : add Copy Model

---
+++ app/Models/Copy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Copy extends Model
+{
+    protected $fillable = [
+        'cookbook_i',
+        'status',
+        'condition',
+    ];
+    
+}
