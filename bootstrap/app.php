7fc4b88560622e16ed80ec70a6fdf23f36cc94d3
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-16 12:28:14 +0000

    feat : add middleware for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\isAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             EnsureFrontendRequestsAreStateful::class,
         ]);
+
+        $middleware->alias([
+            'admin' => isAdmin::class
+        ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\BorrowController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CookBookController;
 use App\Http\Controllers\CopyController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StatisticsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,37 +16,46 @@ Route::get('/user', function (Request $request) {
 //for auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-//for category
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::patch('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-//for cookbook
-Route::get('/cookbooks', [CookBookController::class, 'index']);
-Route::post('/cookbooks', [CookBookController::class, 'store']);
-Route::patch('/cookbooks/{id}', [CookBookController::class, 'update']);
-Route::delete('/cookbooks/{id}', [CookBookController::class, 'destroy']);
-Route::get('/categories/{id}/cookbooks', [CookBookController::class, 'byCategory']);
-Route::get('/categories/{id}/cookbooks/popular', [CookbookController::class, 'mostPopular']);
-Route::get('/categories/{id}/cookbooks/new', [CookbookController::class, 'newArrivals']);
-//for copy
-Route::post('/copies', [CopyController::class, 'store']);
-Route::get('/cookbooks/{id}/copies', [CopyController::class, 'cookbookCopies']);
-Route::patch('/copies/{id}', [CopyController::class, 'update']);
-Route::delete('/copies/{id}', [CopyController::class, 'destroy']);
 
-//for dashboard
-Route::get('/statistics', [StatisticsController::class, 'index']);
-Route::get('/cookbooks/degraded', [StatisticsController::class, 'degradedBooks']);
+Route::middleware('auth:sanctum')->group(function () {
+    //for category
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::patch('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-//for borrow
-Route::post('/borrows', [BorrowController::class, 'store']);
-Route::patch('/borrows/{id}/return', [BorrowController::class, 'returnBook']);
-Route::get('/borrows', [BorrowController::class, 'index']);
+    //for cookbook
+    Route::get('/cookbooks', [CookBookController::class, 'index']);
+    Route::get('/cookbooks/search', [CookbookController::class, 'search']);
+    Route::get('/categories/{id}/cookbooks/popular', [CookbookController::class, 'mostPopular']);
+    Route::get('/categories/{id}/cookbooks/new', [CookbookController::class, 'newArrivals']);
+    Route::get('/categories/{id}/cookbooks', [CookBookController::class, 'byCategory']);
 
-// Route::middleware('auth:sanctum')->group(function () {
+    //for borrow
+    Route::post('/borrows', [BorrowController::class, 'store']);
+    Route::patch('/borrows/{id}/return', [BorrowController::class, 'returnBook']);
+    Route::get('/borrows', [BorrowController::class, 'index']);
 
-// });
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+
+    Route::post('/cookbooks', [CookBookController::class, 'store']);
+    Route::patch('/cookbooks/{id}', [CookBookController::class, 'update']);
+    Route::delete('/cookbooks/{id}', [CookBookController::class, 'destroy']);
+
+    //for copy
+    Route::post('/copies', [CopyController::class, 'store']);
+    Route::get('/cookbooks/{id}/copies', [CopyController::class, 'cookbookCopies']);
+    Route::patch('/copies/{id}', [CopyController::class, 'update']);
+    Route::delete('/copies/{id}', [CopyController::class, 'destroy']);
+
+    //for dashboard
+    Route::get('/statistics', [StatisticsController::class, 'index']);
+    Route::get('/cookbooks/degraded', [StatisticsController::class, 'degradedBooks']);
+
+});
+
 
 Route::get('/test', function () {
     return "API working";
